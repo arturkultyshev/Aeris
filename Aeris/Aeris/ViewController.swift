f0012fee7fd7001881162fd1b2696a466eb3767e
arturkultyshev <kultyshev.artur@gmail.com>
2025-12-05 12:24:32 +0500

    initial commit

---
+++ Aeris/Aeris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aeris
+//
+//  Created by Артур Култышев on 05.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
